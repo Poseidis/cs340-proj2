12e325305fcaf48f8098c8581e81fcf840fdcb5f
Jeffrey Li Wu <jlw7421@moore.wot.eecs.northwestern.edu>
2024-02-09 15:20:30 -0600

    Selective repeat attempt

--- streamer.py
+++ streamer.py
@@ -6,7 +6,7 @@ from struct import pack, unpack
 from concurrent.futures import ThreadPoolExecutor
 import time
 from hashlib import md5
-from threading import Timer
+from threading import Timer, Lock
 
 
 class Streamer:
@@ -23,26 +23,25 @@ class Streamer:
         self.sequence_number = 0
         self.expected_sequence_number = 0
         self.expected_ack_number = 0
-        self.times = {}
-        self.timeout = time.time() + 0.5
-        self.sent_buffer = {}
         self.receive_buffer = {}
         self.ack = False
-        # self.ack_buffer = set()
+        self.ack_buffer = set()
         self.finished = False
         self.closed = False
-
-        self.timer = None
+        self.window_size = 30
+        self.lock = Lock()
 
         # Start listener function
         self.executor = ThreadPoolExecutor(max_workers=1)
         self.executor.submit(self.listener)
 
+
     def listener(self) -> None:
         while not self.closed: 
             try:
                 # Get data and check headers
-                data, addr = self.socket.recvfrom() # BLOCKS HERE
+                data, addr = self.socket.recvfrom()
+                time.sleep(0.01)
 
                 if data:
                     hash = data[0:16] # unpack("!32s", --- )[0]
@@ -54,25 +53,22 @@ class Streamer:
                         curr_seq_num = unpack("!i", data[16:20])[0]
                         is_data = unpack("!?", data[20:21])[0]
                         body = data[21:]
-    
+
+                        
                         if is_data:
                             self.receive_buffer[curr_seq_num] = body
-
                             # Sequence number is an integer
                             # True = data, False = ACK
-                            if self.expected_sequence_number == curr_seq_num:
-                                acknowledgement = pack("!i", curr_seq_num) + pack("!?", False) + body
-                                acknowledgement = bytes.fromhex(md5(acknowledgement).hexdigest()) + acknowledgement
-                                self.socket.sendto(acknowledgement, (self.dst_ip, self.dst_port))
-                               
+                            acknowledgement = pack("!i", curr_seq_num) + pack("!?", False) + body
+                            acknowledgement = bytes.fromhex(md5(acknowledgement).hexdigest()) + acknowledgement
+                            self.socket.sendto(acknowledgement, (self.dst_ip, self.dst_port))
                         else:
                             # self.ack = True
-                            ### self.ack_buffer.add(curr_seq_num)
-                            ## if self.expected_ack_number == curr_seq_num:
-                            ##     self.expected_ack_number += 1
-                            self.timer.cancel()
-                            self.timer = Timer()
-                            self.timer.start()
+
+                            # if curr_seq_num < self.expected_sequence_number + self.window_size:
+                            self.lock.acquire()
+                            self.ack_buffer.add(curr_seq_num)
+                            self.lock.release()
 
                             if body == b"FIN":
                                 self.finished = True
@@ -81,14 +77,31 @@ class Streamer:
                 print("listener died!")
                 print(e)
 
-    def resendPackets(self):
-        for seq_num, packet in self.sent_buffer:
-            
-
+    def sendWithTimer(self, sequence_number, packet):
+        print(sequence_number)
+        print(self.ack_buffer)
+        if sequence_number in self.ack_buffer:
+            self.lock.acquire()
+            self.ack_buffer.remove(sequence_number)
+            if self.expected_ack_number <= sequence_number:
+                self.expected_ack_number = sequence_number + 1
+            self.lock.release()
+            exit()
+        else:
+            if sequence_number >= self.expected_ack_number:
+                self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+                Timer(1, self.sendWithTimer, (sequence_number, packet)).start()
+                time.sleep(0.01)
+        
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
         # Your code goes here!  The code below should be changed!
+
+        # while self.expected_ack_number - self.sequence_number > self.window_size:
+        #     time.sleep(0.01)
+        #     print(self.expected_ack_number)
+        #     print(self.sequence_number)
 
         max_packet_size = 1472 - 4 - 1 - 16    # max_packet_size = 1472 - 4 - 1 - 32
         for i in range(0, len(data_bytes), max_packet_size): 
@@ -97,15 +110,17 @@ class Streamer:
             packet = pack("!i", self.sequence_number) + pack("!?", True) + data_bytes[i:i + max_packet_size]
             packet = bytes.fromhex(md5(packet).hexdigest()) + packet
 
-            self.sent_buffer[self.sequence_number] = packet
-            self.times[self.sequence_number] = time.time()
-            self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+            while self.expected_ack_number - self.sequence_number > self.window_size:
+                time.sleep(0.01)
+                # print(self.sequence_number)
+            # time.sleep(0.1)
+
+            self.sendWithTimer(self.sequence_number, packet)
+            # self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+            self.ack = False
             self.sequence_number += 1
 
-            # self.ack = False
-
-            # self.timer = Timer(0.5, )
-            # self.timer.start()
+            # time.sleep(0.01)
 
             # timeout = time.time() + 0.25
             # while not self.ack:
@@ -118,28 +133,10 @@ class Streamer:
             #         break
                 
 
-
-            # chunks.append(packet)
-
-        # l = len(data_bytes)
-        # i = 0
-        # chunks = []
-        # while l > max_packet_size:
-        #     chunks.append(data_bytes[i:i+max_packet_size])
-        #     l -= max_packet_size
-        #     i += max_packet_size
-        # chunks.append(data_bytes[i:])
-        
-        # for chunk in chunks:
-        #     self.socket.sendto(chunk, (self.dst_ip, self.dst_port))
-            # for now I'm just sending the raw application-level data in one UDP payload
-            # self.socket.sendto(data_bytes, (self.dst_ip, self.dst_port))
-
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
         # your code goes here!  The code below should be changed!
         
-
         while True:
             # data, addr = self.socket.recvfrom()
             # curr_seq_num = unpack("!i", data[0:4])[0]
@@ -150,7 +147,8 @@ class Streamer:
             # print(self.receive_buffer)
 
             data = b""
-            if self.expected_sequence_number in self.receive_buffer:
+            while self.expected_sequence_number in self.receive_buffer:
+                print(self.receive_buffer)
                 packet_data = self.receive_buffer.pop(self.expected_sequence_number)
                 self.expected_sequence_number += 1
                 # return packet_data
@@ -158,6 +156,8 @@ class Streamer:
 
             if data:
                 return data
+
+            time.sleep(0.1)
 
             # else:
             #     self.receive_buffer[curr_seq_num] = data[4:]
@@ -171,10 +171,14 @@ class Streamer:
         # For now, I'll just pass the full UDP payload to the app
         # return data
 
+
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
         # your code goes here, especially after you add ACKs and retransmissions.
+
+        while len(self.ack_buffer) != 0:
+            time.sleep(0.1)
 
         # if not self.ack:
         #     fin_packet = pack("!i", self.sequence_number) + pack("!?", False) + b"FIN"
@@ -195,4 +199,3 @@ class Streamer:
 
         self.closed = True
         self.socket.stoprecv()
-
